eb1620ad7f65d7bfb3c1547162f55e703295f6a2
minecrafting-123 <bright.zheng@thinksign.com>
2025-01-16 11:08:19 -0500

    Google sheets integration & mapbox geocoding

--- index.ts
+++ index.ts
@@ -6,47 +6,80 @@
     Situations with excess students in either groupS
     
 */
-var munkres = require('munkres-js')
-const COOKIE = '__RequestVerificationToken_OnSuite=sTPrdBLGjSVASHp1BAvuydD0jYCjrRWAxKfuj5-QmhqILnVbOhg5KH9lNkq_RFOROIY6EqUcPaM4jJXVJhFqr1QGctCqi3E8femRxGewHXs1; __RequestVerificationToken_OnSuite_TokenId=cca5e046-19e3-4102-9dbd-1ef1767ee32e; _ga=GA1.1.553677966.1728414925; ck=a=+RMOokPN1rM=; bridge=action=create&src=api&xdb=false; t=fbe950e4-7911-84e2-ee0c-2ce2b1e5eadc; sd=e3cf8bc0-6e00-40b4-958a-2525dda711aa; persona=student; _ga_GBR1P7H50V=GS1.1.1728414924.1.1.1728414966.0.0.0; userDataSessionID=feb6bced-dfc4-433b-99ec-7f101c2e7a0a%7C216659c9-7e84-4b82-848f-6ec4eb3186c0; AuthSvcToken=cYS%2FmOEP59HaCw1v3r0UMAgs3G3VkEP35avW9RyuTidgV5NP8g2oP6EvUwb7vC%2F4S0Pb4cLZcFl4ovuSGB1bgA1OU%2BXTHeXAGjvQzTIvyOSn0GhiFpfFR9QJrb6JJ99EmKLAmgH97TpoeabSQdVM7DqtNrZtcRP0mDfM3mtLqTeo974p73MEW4ZK0X1jdDW%2Fde14%2BCdFccoaBbIofpiu%2FSGbQGtZyKm1ZCl9SEuNLZ8kUa%2BQdy8jSgh1Q37xkb9SiBBBwNLJcsvz1iTsA0TBnBZJoSITGME937zvIlqbQHbrVftadC10PLP74aCofEXr%2BkHzV5FxyuGdTcGM%2BYlnxQ%3D%3D.H4sIAAAAAAAAA51STY%2FaMBAN%2FdC2tFLb%2B1bi0GPJFyEkSFULCUshS2lJyO5yWRnbSbwksRsnsPTX12m4VKp6wLJ98Lx5b%2BZ5nkiS9G7EWEogKAnNHUp3BLekltQWETcpSzZUFA4TnAEuP2Ypp4DJtIiVA1d0Ve0rYhOE85KURwWmgGRcyUGGm7eI4OIDUk0L2dagCxHCXcMEg66Nt2rXwEZkIWibPf2PYksan6cnsCQFCBWY88ttQeKklH8lOI%2B%2F8LKqsfIOopqkUXk745M6w6F5RIoMo2dBUZ0q%2BHxeBTHZ47xu%2B%2BIroBnJT0qfzmPjVVFzPd%2FUPTRML69pTHIXlLitDXTLEJmG3YTajr%2B6ug%2BW3uRboM2PDCLc30bTeTb14sCaLKuC%2Btcs5DdJz1mkleemfKKF4V2YrjdW5N1tR%2FbVPp0b2s99ML6h4W7s58F4GmUM%2BHZyc3tS8YW5YkDuZyh46KlhQtfuD%2FDddOY9j1SDUeBNPYtZleGUZuD33XW1cGJtA9yJeTDCbeWFdIWMBxhqi5lVQtq%2FjUIWCqd2czTTN7BRed3Mn0vF%2F%2BRvsqMwjtIUMCZDmjWQVyuc4wNI%2F%2FLCtJpgver7qTj%2F5bqQCecVRu%2BDCn%2FsqFZnCcuOrupGR%2BsPNXOoDjrTRfBCxo%2BMiLFqYLr%2Bb9hvmDkZKEUDAAA%3D';
-const TWELFTH_FACET = "9536_Twelfth Grade";
-const ELEVENTH_FACET = "9536_Eleventh Grade";
+const munkres = require('munkres-js')
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+
+process.loadEnvFile('.env');
+
 const KCD_LONG = -85.6689
 const KCD_LAT = 38.2423
 
-type UserEntry = {
-  UserID: string;
-  FirstName: string;
-  LastName: string;
-  PublishUserProfile: boolean;
-  LargeFileName: string;
-  Email: string;
-  EmailBad: boolean;
-  PublishEmail: boolean;
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
-  AddressLine1: string;
-  City: string;
-  Zip: string;
-  HomePhone: string; // trim if used
-  MyContactsId: number;
-
-  HasRelationships: boolean; // no idea what this is for
-  GradYear: string;
-  Grade: string; // empty!
-  GradeDisplay: string; // use this instead of Grade
-  Department: string;
-  SpouseId: number;
-  GradeNumeric: number; // always 0!
-  GradeNumericDisplay: string; // use this instead of ^
-  TotalCount: number; // ??
-  Deceased: boolean; // !!?
-  IsStudentInd: boolean; // ?
-
-  PreferredAddressId: number;
-  PreferredAddressLat: number;
-  PreferredAddressLng: number;
-
-  JobTitle: string; // empty
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
 }
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+\\
 
 function getDistanceFromLatLonInMi(lat1, lon1, lat2, lon2) {
   var R = 3959; // Radius of the earth in miles
@@ -64,33 +97,6 @@ function getDistanceFromLatLonInMi(lat1, lon1, lat2, lon2) {
 
 function deg2rad(deg: number){
   return deg * (Math.PI/180)
-}
-
-function fetchGrade(facet: string) {
-  // encode and fetch
-  facet = encodeURIComponent(facet);
-  return fetch(`https://kcd.myschoolapp.com/api/directory/directoryresultsget?directoryId=1879&searchVal=&facets=${facet}&searchAll=false`, {
-    "headers": {
-      "accept": "application/json, text/javascript, */*; q=0.01",
-      "accept-language": "en-US,en;q=0.9",
-      "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Linux\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "wh-version": "2024.10.07.1",
-      "x-requested-with": "XMLHttpRequest",
-      "cookie": COOKIE,
-      "Referer": "https://kcd.myschoolapp.com/app/student",
-      "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-    "body": null,
-    "method": "GET"
-  }).then(res => res.json() as Promise<UserEntry[]>)
-  .catch(e => {
-    console.log(e)
-  })
 }
 
 async function findOutliers() {
@@ -113,16 +119,117 @@ async function findOutliers() {
   return outliers;
 }
 
-async function getData() {
-  //aggregate data into junior and senior matricies
-  //data for juniors
-  const juniors = (await fetchGrade(ELEVENTH_FACET)).map(it => 
-    ({ name: `${it.FirstName} ${it.LastName}`, lat: it.PreferredAddressLat, long: it.PreferredAddressLng }));
-  //data for seniors
-  const seniors = (await fetchGrade(TWELFTH_FACET)).map(it => 
-    ({ name: `${it.FirstName} ${it.LastName}`, lat: it.PreferredAddressLat, long: it.PreferredAddressLng }));
-  return [juniors, seniors]
+async function geocode(row) {
+  var address = row[3]
+  if (address.includes("P.O.")){
+    address = row[6].concat(" post office")
+  }
+  //const response = await limiter.schedule(() => fetch(`https://nominatim.openstreetmap.org/search?addressdetails=0&q=${address.replaceAll(" ","+")}+Kentucky&format=jsonv2&limit=1&viewbox=${KCD_LONG-1},${KCD_LAT-1},${KCD_LONG+1},${KCD_LAT+1}&bounded=1`));
+  //const json = await response.json();
+  //not on openstreetmap.org, we try MapBox
+  const mapResponse = await fetch(`https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(address)}&bbox=${KCD_LONG-1},${KCD_LAT-1},${KCD_LONG+1},${KCD_LAT+1}&access_token=${process.env.MAPBOX_TOKEN}`)
+  const mapJson = await mapResponse.json();
+  console.log(mapJson["features"][0]["geometry"]["coordinates"])
+  return [mapJson["features"][0]["geometry"]["coordinates"][0], mapJson["features"][0]["geometry"]["coordinates"][1]]
 }
+
+async function processCoords(array){
+  const promises = array.map(address => geocode(address))
+  const results = await Promise.all(promises)
+  return results
+}
+
+function formatCoords(array, juniors, seniors){
+  let inputStr = Array<string>()
+  const juniorGroups = Math.ceil(juniors / 25)
+  const seniorGroups = Math.ceil(seniors / 25)
+  //handling junior data
+  for (let i = 0; i < juniorGroups; i++) {
+    let coordstr = ""
+    //if it's the last group, go until the end, which is less than 25 entries
+    if (i+1 == juniorGroups){
+      //console.log(coordstr)
+      for (let j = i*25; j < array.length; j++){
+        //console.log("HERE!" + array[j][0].toString())
+        coordstr = coordstr.concat(`${array[j][0].toString()},${array[j][1].toString()};`)
+      }
+      inputStr.push(coordstr.slice(0,-1))
+      break
+    }
+    for (let j = 0; j < 25; j++) {
+      coordstr = coordstr.concat(`${array[i*25 + j][0].toString()},${array[i*25 + j][1].toString()};`)
+    }
+    inputStr.push(coordstr.slice(0,-1))
+    //console.log('coordstring gaming: ' + coordstr)
+  }
+  console.log(inputStr)
+  
+  return inputStr
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+async function listAddresses(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: '1XtZwd1i4ih5OFrQdv747OCMiDOc8889iV3DD4_KMLg4',
+    range: 'Data Import!A2:L30',
+  });
+  const rows = res.data.values;
+  if (!rows || rows.length === 0) {
+    console.log('No data found.');
+    return;
+  }
+  console.log('Name, Address:');
+  let addresses = Array<string[]>()
+  let problems = Array<string[]>()
+
+  //variables for processing data while looping
+  let juniorCount = 0
+  let seniorCount = 0
+  let prevYear = null
+  let isSenior = false
+
+  rows.forEach((row) => {
+    //set year for juniors at beginning
+    if (prevYear == null){
+      prevYear = row[2]
+    }
+
+    if (prevYear != row[2]){
+      isSenior = true
+    }
+    prevYear = row[2]
+
+    const address = row[3]
+    // TODO: store the problem addresses for later so manual input is ballin
+    // IMPORTANT: counts don't include the people 
+    if (address != undefined && (address.length > 1)){
+      if (isSenior){
+        seniorCount += 1
+      } else {
+        juniorCount += 1
+      }
+      addresses.push(row)
+    } else {
+      problems.push(row)
+    }
+  });
+  //console.log(addresses);
+  
+  const rawCoords = await processCoords(addresses)
+  console.log("lat, long:")
+  console.log(rawCoords)
+  console.log('first raw coord: ' + rawCoords[0])
+  console.log(`Juniors ${juniorCount}, Seniors ${seniorCount}`)
+  
+  const coordinateStrings = formatCoords(rawCoords, juniorCount, seniorCount)
+  console.log(coordinateStrings)
+
+};
 
 async function calculate() {
   let [juniors, seniors] = await getData()
@@ -139,6 +246,7 @@ async function calculate() {
   }))
 }
 
+authorize().then(listAddresses).catch(console.error);
 (async () => {
   console.log(await calculate())
 })();
